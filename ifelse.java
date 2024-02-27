78ef2f47cd7aa1d194fa0a8cf2ae0dfc4d6e205f
RychuComp <161369537+RychuComp@users.noreply.github.com>
2024-02-27 16:59:58 +0100

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+class ifelse {
+  public static void main(String[] args) {   
+    int num = 14;
+    
+    if(num >= 15) {
+        System.out.println("Good choice: " + num);
+    } 
+    else{
+         System.out.println("ups that's incorrect: " + num);
+    }
+
+  }
+}
