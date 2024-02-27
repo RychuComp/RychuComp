748531f4e82a6222cb711153a54d6af1fa5b225c
RychuComp <161369537+RychuComp@users.noreply.github.com>
2024-02-27 16:57:01 +0100

    Create bools.java
    :D

---
+++ bools.java
@@ -0,0 +1,18 @@
+class bools {
+ public static void main(String[] args) {
+    int num1 = 2;
+    double num2 = 4.6;
+    int num3 = 5;
+    int num4 = 26;
+    
+    boolean result1 = num1 < num2;
+    boolean result2 = num3 > num1;
+    boolean result3 = num3 != num1;
+    boolean result4 = num1 <= num4;
+    
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+    System.out.println(result4);
+ }    
+}
