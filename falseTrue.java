1709d0b3d42860b111a670b5d0b56048b1a2749d
RychuComp <161369537+RychuComp@users.noreply.github.com>
2024-02-27 16:58:09 +0100

    Create falseTrue.java
    🗡️

---
+++ falseTrue.java
@@ -0,0 +1,8 @@
+class falseTrue {
+    public static void main(String[] args) {
+        int num = 2;
+        boolean result = num < 6; //true
+        
+        System.out.println(result);
+    }
+}
